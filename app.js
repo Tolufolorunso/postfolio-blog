cab906cd8cfc1cb03fa1138fb13a8367b527313e
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-06-14 12:51:43 +0100

    bug/css fixed

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get("*", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
