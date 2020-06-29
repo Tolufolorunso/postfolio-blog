12a367b392de7bc1f1ed416daebed3d2f0505c83
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-06-29 12:56:16 +0100

    done with crud part2

--- routes/adminCrudRoutes.js
+++ routes/adminCrudRoutes.js
@@ -8,6 +8,10 @@ router.get("/", authorize, adminCrud.getAdminDashboard);
 router.get("/posts/new", authorize, adminCrud.showForm);
 router.get("/posts", adminCrud.getAllPosts);
 router.post("/posts", adminCrud.postToDb);
+router.get("/posts/:postId/edit", authorize, adminCrud.getPostToEdit);
+
+router.put("/posts/:postId", adminCrud.updatePost);
+
 router.delete("/posts/:postId", adminCrud.deletePost);
 
 module.exports = router;
